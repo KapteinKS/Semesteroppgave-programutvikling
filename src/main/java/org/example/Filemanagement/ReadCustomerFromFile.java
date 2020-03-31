f99717057c049e85bf3fa0f9ee89575655905825
Henrik Stangeland <henrik.b.stangeland@gmail.com>
2020-03-31 15:49:54 +0200

    Merge branch 'master' of https://github.com/KapteinKS/Semesteroppgave-programutvikling
    # Conflicts:
    #	src/main/java/org/example/Checker.java

--- src/main/java/org/example/Filemanagement/ReadCustomerFromFile.java
+++ src/main/java/org/example/Filemanagement/ReadCustomerFromFile.java
@@ -1,4 +1,40 @@
 package org.example.Filemanagement;
 
+
+// This handles TXT.
+
+import org.example.Deeper.Customer;
+import org.example.Deeper.CustomerCollection;
+import org.example.Exceptions.InvalidCustomerException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ReadCustomerFromFile extends Reader {
+
+	public void open(CustomerCollection customerCollection, Path customerFilePath) throws IOException {
+		customerCollection.removeAll();
+		try (BufferedReader bufferedReader = Files.newBufferedReader(customerFilePath)){
+			String line;
+			while ((line = bufferedReader.readLine()) != null){
+				customerCollection.addCustomer(parseCustomer(line));
+			}
+		}
+		}
+
+
+		public Customer parseCustomer(String line) throws InvalidCustomerException {
+		String[] split = line.split(",");
+		if (split.length != 5){
+			throw new InvalidCustomerException("ERROR! Values not separated by ,");
+		}
+		String name = split[0];
+		String email = split[3];
+		String phone = split[4];
+
+		};
 }
