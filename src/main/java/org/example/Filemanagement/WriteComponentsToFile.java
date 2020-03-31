b043b353b47e14e1f79b74c6fc13aa91f1fa54df
matsovevada <55790581+matsovevada@users.noreply.github.com>
2020-03-31 13:00:55 +0200

    Update WriteComponentsToFile.java

--- src/main/java/org/example/Filemanagement/WriteComponentsToFile.java
+++ src/main/java/org/example/Filemanagement/WriteComponentsToFile.java
@@ -1,8 +1,8 @@
 package org.example.Filemanagement;
 
-import javafx.scene.shape.Path;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class WriteComponentsToFile {
