c9d5773a9751a5f36419ca794eff882e07dde2ac
matsovevada <55790581+matsovevada@users.noreply.github.com>
2020-04-15 11:56:42 +0200

    Create RAM.java

---
+++ src/main/java/org/example/components/RAM.java
@@ -0,0 +1,45 @@
+package org.example.components;
+
+import javafx.beans.property.SimpleStringProperty;
+
+import java.io.Serializable;
+
+public class RAM extends Component implements Serializable {
+    private SimpleStringProperty memory;
+    private SimpleStringProperty amountOfRAMPieces;
+
+    public RAM(SimpleStringProperty memory, SimpleStringProperty amountOfRAMPieces) {
+        this.memory = memory;
+        this.amountOfRAMPieces = amountOfRAMPieces;
+    }
+
+    public RAM(String name, String manufacturer, double price, SimpleStringProperty memory, SimpleStringProperty amountOfRAMPieces) {
+        super(name, manufacturer, price);
+        this.memory = memory;
+        this.amountOfRAMPieces = amountOfRAMPieces;
+    }
+
+    public String getMemory() {
+        return memory.get();
+    }
+
+    public SimpleStringProperty memoryProperty() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory.set(memory);
+    }
+
+    public String getAmountOfRAMPieces() {
+        return amountOfRAMPieces.get();
+    }
+
+    public SimpleStringProperty amountOfRAMPiecesProperty() {
+        return amountOfRAMPieces;
+    }
+
+    public void setAmountOfRAMPieces(String amountOfRAMPieces) {
+        this.amountOfRAMPieces.set(amountOfRAMPieces);
+    }
+}
