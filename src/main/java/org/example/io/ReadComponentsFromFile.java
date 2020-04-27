9f2ad7e3c55942a15e280fd30b2f98b1701b1757
matsovevada <55790581+matsovevada@users.noreply.github.com>
2020-04-27 12:27:58 +0200

    Filewriter for components

--- src/main/java/org/example/io/ReadComponentsFromFile.java
+++ src/main/java/org/example/io/ReadComponentsFromFile.java
@@ -1,4 +1,11 @@
 package org.example.io;
 
+import org.example.Deeper.ComponentCollection;
+import org.example.components.Component;
+
+import java.nio.file.Path;
+
+
 public class ReadComponentsFromFile extends Reader {
+
 }
