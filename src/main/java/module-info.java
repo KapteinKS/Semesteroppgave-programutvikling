e891ab20b73195b3e3ecdf924ab0ee8c3e084723
KapteinKS <sander.toien@outlook.com>
2020-04-27 14:28:51 +0200

    Revert "Revert "Work on tableview""
    This reverts commit a43be32f61a45267f78f6e8b6867fb4443bf54d7.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,6 @@ module org.example {
     requires javafx.fxml;
 
     opens org.example to javafx.fxml;
+    opens org.example.components to javafx.base;
     exports org.example;
 }
--- src/main/java/org/example/components/Component.java
+++ src/main/java/org/example/components/Component.java
@@ -1,41 +1,42 @@
 package org.example.components;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 import java.io.Serializable;
 
 public class Component implements Serializable {
-	private String name;
-	private String manufacturer;
-	private double price;
-
-	public Component(){}
+	private transient SimpleStringProperty name;
+	private transient SimpleStringProperty manufacturer;
+	private transient SimpleDoubleProperty price;
 
 	public Component(String name, String manufacturer, double price){
-		this.name = name;
-		this.manufacturer = manufacturer;
-		this.price = price;
+		this.name = new SimpleStringProperty(name);
+		this.manufacturer = new SimpleStringProperty(manufacturer);
+		this.price = new SimpleDoubleProperty(price);
 	}
 
 	public String getName(){
-		return name;
+		return name.getValue();
 	}
 
 	public void setName(String name){
-		this.name = name;
+		this.name.set(name);
 	}
 
 	public double getPrice() {
-		return price;
+		return price.getValue();
 	}
 
 	public void setPrice(double price){
-		this.price = price;
+		this.price.set(price);
 	}
 
 	public String getManufacturer(){
-		return manufacturer;
+		return manufacturer.getValue();
 	}
 
 	public void setManufacturer(String manufacturer){
-		this.manufacturer = manufacturer;
+		this.manufacturer.set(manufacturer);
 	}
 }
