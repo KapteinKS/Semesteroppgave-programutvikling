942df1c81ae2bbba67d07664beceb495489f242e
Henrik Stangeland <henrik.b.stangeland@gmail.com>
2020-04-29 19:22:05 +0200

    Update ComponentCollection.java

--- src/main/java/org/example/logicAndClasses/ComponentCollection.java
+++ src/main/java/org/example/logicAndClasses/ComponentCollection.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
 import org.example.componentClasses.Component;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ComponentCollection {
@@ -21,6 +22,36 @@ public class ComponentCollection {
 
 	public void attachTableView(TableView tv){
 		tv.setItems(componentList);
+	}
+
+	public static ObservableList<Component> filter(String choiceBoxValue, String filterInput){
+		List<Component> filteredList;
+		switch (choiceBoxValue){
+			case "Produsent": {
+				filteredList = componentList.parallelStream().filter(p -> (p.getManufacturer().startsWith(filterInput)))
+						.collect(Collectors.toCollection(FXCollections::observableArrayList));
+				return (ObservableList<Component>) filteredList;
+			}
+			case "Navn": {
+				filteredList = componentList.parallelStream().filter(p -> (p.getName().startsWith(filterInput)))
+						.collect(Collectors.toCollection(FXCollections::observableArrayList));
+				return (ObservableList<Component>) filteredList;
+			}
+			case "Høyeste Pris": {
+				filteredList = componentList.parallelStream()
+						.filter(p -> ((p.getPrice() <= Double.parseDouble(filterInput))))
+						.collect(Collectors.toCollection(FXCollections::observableArrayList));
+				return (ObservableList<Component>) filteredList;
+			}
+			case "Type": {
+				filteredList = componentList.parallelStream().filter(p -> (p.getType().toLowerCase().startsWith(filterInput.toLowerCase())))
+						.collect(Collectors.toCollection(FXCollections::observableArrayList));
+				return (ObservableList<Component>) filteredList;
+			}
+			default:
+				return componentList;
+		}
+
 	}
 
 	public ObservableList<Component> getComponentByType(String type){
