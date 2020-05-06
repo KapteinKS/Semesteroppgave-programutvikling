d71669032e3421aabc156423c77fa57bc58b223b
matsovevada <55790581+matsovevada@users.noreply.github.com>
2020-05-06 15:24:45 +0200

    CustomerColl, Customer, Ordercollection, filemanagement for orders

--- src/main/java/org/example/io/WriteCustomerToFile.java
+++ src/main/java/org/example/io/WriteCustomerToFile.java
@@ -1,4 +1,16 @@
 package org.example.io;
 
+import org.example.logicAndClasses.CustomerCollection;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+//Saves CUSTOMERS (.txt)
+
 public class WriteCustomerToFile extends Writer {
+
+    public static void save(CustomerCollection customerCollection, Path path) throws IOException {
+        Files.write(path,customerCollection.toString().getBytes());
+    }
 }
--- src/main/java/org/example/logicAndClasses/CustomerCollection.java
+++ src/main/java/org/example/logicAndClasses/CustomerCollection.java
@@ -3,8 +3,28 @@ package org.example.logicAndClasses;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.List;
+
 public class CustomerCollection {
 
-	private ObservableList<Object> list = FXCollections.observableArrayList();
+	private ObservableList<Customer> customers = FXCollections.observableArrayList();
 
+	public List<Customer> getCustomers(){
+		return customers;
+	}
+	public void addCustomer(Customer c){
+		customers.add(c);
+	}
+	public void removeAll(){
+		customers.clear();
+	}
+
+	@Override
+	public String toString(){
+		String out = "";
+		for (Customer c : customers){
+			out += c.toString();
+		}
+		return out;
+	}
 }
