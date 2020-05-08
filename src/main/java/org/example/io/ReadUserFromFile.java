69f89bba1b3659133647d2d866a7de8cf8596761
KapteinKS <sander.toien@outlook.com>
2020-05-08 14:29:41 +0200

    don't know why these weren't pushed

--- src/main/java/org/example/io/ReadUserFromFile.java
+++ src/main/java/org/example/io/ReadUserFromFile.java
@@ -1,53 +1,33 @@
 package org.example.io;
 
-
-// This handles .txt files.
-
-import org.example.logicAndClasses.EndUser;
 import org.example.logicAndClasses.User;
 import org.example.logicAndClasses.UserCollection;
-import org.example.exceptions.InvalidCustomerException;
-
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ReadCustomerFromFile extends Reader {
+public class ReadUserFromFile extends Reader {
+	public static UserCollection open() throws IOException, ClassNotFoundException {
+		Path path = Paths.get("users.jobj");
+		try (InputStream in = Files.newInputStream(path);
+			 ObjectInputStream oin = new ObjectInputStream(in)){
+			List<User> users = (ArrayList<User>) oin.readObject();
 
-	public static void open(UserCollection userCollection, Path customerFilePath) throws IOException {
-		userCollection.removeAll();
-		try (BufferedReader bufferedReader = Files.newBufferedReader(customerFilePath)) {
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				userCollection.addCustomer(parseCustomer(line));
+			UserCollection userCollection = new UserCollection();
+
+			for(User u : users){
+				userCollection.addUser(u);
 			}
-		} catch (InvalidCustomerException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static User parseCustomer(String line) throws InvalidCustomerException {
-		String[] split = line.split(",");
-		if (split.length != 9) {
-			throw new InvalidCustomerException("ERROR! Values not separated by ',' !!");
-		}
-
-		//We could do some checks on some of these, but we should keep them as Strings, as we're not calculating ever.
-		String customerId = split[0];
-		String firstName = split[1];
-		String lastName = split[2];
-		String address = split[3];
-		String postalCode = split[4];
-		String postalArea = split[5];
-		String phoneNumber = split[6];
-		String email = split[7];
-		String password = split[8];
-
-		try {
-			return new EndUser(customerId, firstName, lastName, address, postalCode, postalArea, phoneNumber, email, password);
-		} catch (IllegalArgumentException e) {
-			throw new InvalidCustomerException(e.getMessage());
+			return userCollection;
+		} catch (IOException ioe){
+			throw new IOException("Something went wrong reading from file");
+		} catch (ClassNotFoundException c){
+			throw new ClassNotFoundException("File reader couldn't find a class");
 		}
 	}
 }
--- src/main/java/org/example/io/WriteUserToFile.java
+++ src/main/java/org/example/io/WriteUserToFile.java
@@ -1,16 +1,26 @@
 package org.example.io;
 
+import org.example.logicAndClasses.User;
 import org.example.logicAndClasses.UserCollection;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 //Saves CUSTOMERS (.txt)
 
-public class WriteCustomerToFile extends Writer {
+public class WriteUserToFile extends Writer {
 
-    public static void save(UserCollection userCollection, Path path) throws IOException {
-        Files.write(path, userCollection.toString().getBytes());
+    public static void save(List<User> users) throws IOException {
+        Path path = Paths.get("users.jobj");
+        try(OutputStream os = Files.newOutputStream(path);
+            ObjectOutputStream out = new ObjectOutputStream(os)){
+            out.writeObject(new ArrayList<>(users));
+        }
     }
 }
