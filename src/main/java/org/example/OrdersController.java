6ac0cb004f129290be1cb2459cc62d9c5e617999
matsovevada <55790581+matsovevada@users.noreply.github.com>
2020-05-12 13:55:22 +0200

    OrdersGUI HELP ME!!!!!

--- src/main/java/org/example/OrdersController.java
+++ src/main/java/org/example/OrdersController.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import org.example.logicAndClasses.DialogueBoxes;
+import org.example.logicAndClasses.Order;
+import org.example.logicAndClasses.OrderCollection;
 
 import java.io.IOException;
 
 public class OrdersController {
+    private ObservableList <OrderCollection> orders = FXCollections.observableArrayList();
+    String out = "";
 
     @FXML
     private Label lblOrderList;
@@ -19,7 +25,14 @@ public class OrdersController {
 
     @FXML
     void btnOpenOrder(ActionEvent event) {
-
+        for (OrderCollection order : orders) {
+            out += order.printOrders(App.getCurrentUser().getUserID());
+        }
+        if (out.equals("")) {
+            DialogueBoxes.alert("Feil!", "Du har ingen tidligere ordre!");
+        } else {
+            lblOrderList.setText(out);
+        }
     }
 
     @FXML
